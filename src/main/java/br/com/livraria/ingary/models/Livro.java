4f14616ff083af830feff831217c440192c233db
chenr <c.henrique.cunha08@gmail.com>
2022-01-06 18:57:09 -0300

    Adicionando classe livro

---
+++ src/main/java/br/com/livraria/ingary/models/Livro.java
@@ -0,0 +1,26 @@
+package br.com.livraria.ingary.models;
+
+import java.util.Date;
+
+public class Livro {
+
+    String capa;
+    String titulo;
+    String subtitulo;
+    String autor;
+    String coAutor;
+    String ilustracao;
+    int paginacao;
+    String editora;
+    String edicao;
+    String anoLancamento;
+    String genero;
+    String idioma;
+    String capaNormalCapaDura;
+    String CodigoBarra;
+    float preco;
+    int estoque;
+
+
+    }
+}
